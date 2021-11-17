d444c1b6cf9e5ab444282495c793c42fbb81a3ba
Premek Janda <xjanda28@vutbr.cz>
2021-11-17 13:42:05 +0100

    Update AST

--- syntax analyzer/tree.c
+++ syntax analyzer/tree.c
@@ -4,6 +4,98 @@
 #include <string.h>
 #include "tree.h"
 
+int node_init(t_node *node)
+{
+    // inicializace potomků 
+    node->next_capacity = 5;
+    node->next_count = 0;
+    node->next = malloc(sizeof(t_node) * node->next_capacity);
+    ALLOC_CHECK(node->next)
+    node->prev = NULL;
+    
+    // inicializace atributu a typu
+    node->data = malloc(sizeof(t_tree_item) * 2);
+    ALLOC_CHECK(node->data)
+    tree_item_init(&node->data[0]);
+    tree_item_init(&node->data[1]);
+
+    return 0;
+}
+
+int node_setdata(t_node *node, char *data, int index)
+{
+    if (index != 0 && index != 1)
+        return 0;
+
+    // uvolnění paměti, pokud je už prvek plný
+    if (node->data[index].data != NULL)
+        free(node->data[index].data);
+
+    // nastavení nových hodnot
+    node->data[index].data = malloc(strlen(data) + 1);
+    node->data[index].length = strlen(data);
+    ALLOC_CHECK(node->data[index].data)
+    
+    strcpy(node->data[index].data, data);
+    
+    return 0;
+}
+
+int node_addnext(t_node *node, t_node *next)
+{
+    node->next[node->next_count] = *next;   // uloží ukazatel na další prvek
+    (*next).prev = node;                    // nastavení ukazatele na předchozí prvek
+    node->next_count++;
+
+    // pro rozšíření počtu potomků
+    if (node->next_count == node->next_capacity) {
+        node->next_capacity = node->next_capacity * 2;
+    
+        t_node *newnext = realloc(node->next, sizeof(t_node) * node->next_capacity);
+        ALLOC_CHECK(newnext)
+    
+        node->next = newnext;
+    }
+
+    return 0;
+}
+
+void node_free(t_node *node)
+{
+    free(node->next);                   // uvolnění potomků
+    tree_item_delete(&node->data[0]);   //          atributu
+    tree_item_delete(&node->data[1]);   //          typu
+    free(node->data);                   //          struktury tree_item
+}
+
+void node_delete(t_node *node)
+{
+    // rekurze přes všechny potomky
+    for(; node->next_count > 0; node->next_count--) 
+        node_delete(&node->next[node->next_count - 1]);
+    
+    node_free(node);
+}
+
+void node_print(t_node node, size_t tabs)
+{
+    for (size_t i = 0; i < tabs; i++)
+        printf("\t");
+    
+    printf("%s\n", node.data[0].data);
+    // printf("%s %s\n", node.data[0].data, node.data[1].data);
+}
+
+void tree_print(t_node node, size_t tabs)
+{
+    // první se printne root, později s tabulátorem navíc potomci
+    node_print(node, tabs);
+
+    // rekurze přes všechny potomky
+    for(int i = 0; i < node.next_count; i++)
+        tree_print(node.next[node.next_count - i - 1], tabs + 1);
+}
+
 void tree_item_init(t_tree_item *tree_item)
 {
     tree_item->length = 0;
@@ -12,153 +104,59 @@ void tree_item_init(t_tree_item *tree_item)
 
 void tree_item_delete(t_tree_item *tree_item)
 {
-    if(tree_item->data != NULL)
+    if (tree_item->data != NULL)
         free(tree_item->data);
 }
 
-void node_init(t_node *node)
-{
-    node->next_capacity = 5;
-    node->next_count = 0;
-    node->next = malloc(sizeof(t_node) * node->next_capacity);
-    node->prev = NULL;
-    node->data = malloc(sizeof(t_tree_item) * 2);
-    tree_item_init(&node->data[0]);
-    tree_item_init(&node->data[1]);
-}
 
-int node_setdata(t_node *node, char *data, int index)
-{
-    if(index != 0 && index != 1)
-        return 0;
-    if(node->data[index].data != NULL)
-        free(node->data[index].data);
-    node->data[index].data = malloc(strlen(data) + 1);
-    node->data[index].length = strlen(data);
-    if(node->data[index].data == NULL)
-        return 1;
-    strcpy(node->data[index].data, data);
-    return 0;
-}
+// ! /////////////// ! //
+// !     UNUSED      ! //
+// ! /////////////// ! //
+// void tree_init(t_tree *tree)
+// {
+//     tree->first = NULL;
+//     tree->active = NULL;
+// }
 
-int node_copy(t_node *node1, t_node *node2)
-{
-    node1->next_count = node2->next_count;
-    if(node1->next_capacity != node2->next_capacity)
-    {
-        node1->next_capacity = node2->next_capacity;
-        t_node *newnext = realloc(node1->next, sizeof(t_node) * node1->next_capacity);
-        if(newnext == NULL)
-            return 1;
-        node1->next = newnext;
-    }
-    for(int i = 0; node2->next_count; i++)
-        node1->next[i] = node2->next[i];
-    tree_item_init(&node1->data[0]);
-    tree_item_init(&node1->data[1]);
-    if(node2->data[0].length != 0)
-        node_setdata(node1, node2->data[0].data, 0);
-    if(node2->data[1].length != 0)
-        node_setdata(node1, node2->data[1].data, 1);
-    return 0;
-}
+// void tree_setfirst(t_tree *tree, t_node *node)
+// {
+//     t_node new_node;
+//     node_init(&new_node);
+//     node_copy(&new_node, node);
+//     tree->first = &new_node;
+// }
 
-void node_free(t_node *node)
-{
-    free(node->next);
-    tree_item_delete(&node->data[0]);
-    tree_item_delete(&node->data[1]);
-    free(node->data);
-}
+// void tree_delete(t_tree *tree)
+// {
+//     node_delete(tree->first);
+// }
 
-void node_setprev(t_node *node, t_node *prev)
-{
-    node->prev = prev;
-}
+// int node_copy(t_node *node1, t_node *node2)
+// {
+//     node1->next_count = node2->next_count;
 
-void node_addnext(t_node *node, t_node *next)
-{
-    t_node new_node;
-    node_init(&new_node);
-    node_copy(&new_node, next);
-    node_setprev(&new_node, node);
-    node->next[node->next_count] = new_node;
-    node->next_count++;
-    if(node->next_count == node->next_capacity)
-    {
-        node->next_capacity = node->next_capacity * 2;
-        t_node *newnext = realloc(node->next, node->next_capacity);
-        node->next = newnext;
-    }
-}
+//     if (node1->next_capacity != node2->next_capacity)
+//     {
+//         node1->next_capacity = node2->next_capacity;
+//         t_node *newnext = realloc(node1->next, sizeof(t_node) * node1->next_capacity);
+        
+//         if (newnext == NULL)
+//             return 1;
+        
+//         node1->next = newnext;
+//     }
 
-void node_print(t_node node)
-{
-    printf("\nh");
-    printf("\n%s     %s   %d", node.data[0].data, node.data[1].data, node.next_count);
-    printf("\n");
-}
+//     for(int i = 0; node2->next_count; i++)
+//         node1->next[i] = node2->next[i];
 
-void node_delete(t_node *node)
-{
-    for(; node->next_count > 0; node->next_count--)
-    {
-        node_delete(&node->next[node->next_count - 1]);
-    }
-    if(node->next_count == 0)
-    {
-        node_free(node);
-        return;
-    }
-}
+//     tree_item_init(&node1->data[0]);
+//     tree_item_init(&node1->data[1]);
 
-void tree_print(t_node node)
-{
-    for(int i = 0; i < node.next_count; i++)
-    {
-        tree_print(node.next[i]);
-    }
-    node_print(node);
-}
-
-void tree_init(t_tree *tree)
-{
-    tree->first = NULL;
-    tree->active = NULL;
-}
-
-void tree_setfirst(t_tree *tree, t_node *node)
-{
-    t_node new_node;
-    node_init(&new_node);
-    node_copy(&new_node, node);
-    tree->first = &new_node;
-}
-
-void tree_delete(t_tree *tree)
-{
-    node_delete(tree->first);
-}
-
-/*
-int main()
-{
-    printf("smrdi ti odporny kokot\n");
-    t_node node;
-    node_init(&node);
-    node_setdata(&node, "ty geju", 1);
-    t_node node1;
-    node_init(&node1);
-    node_setdata(&node1, "ty geju", 1);
-    t_node node2;
-    node_init(&node2);
-    node_setdata(&node2, "ty geju", 1);
-    node_setdata(&node2, "aaaa", 1);
-
-    node_addnext(&node, &node1);
-    node_addnext(&node, &node2);
-
-
-    node_delete(&node);
-    return 0;
-}*/
+//     if (node2->data[0].length != 0)
+//         node_setdata(node1, node2->data[0].data, 0);
+    
+//     if (node2->data[1].length != 0)
+//         node_setdata(node1, node2->data[1].data, 1);
+    
+//     return 0;
+// }
